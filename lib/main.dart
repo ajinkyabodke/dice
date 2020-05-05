34750b3e1bc8567a2b4865f9ae8d4b486d0d7705
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2020-05-05 17:23:51 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 1;
+  int rightDiceNo = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNo = Random().nextInt(6) + 1;
+      rightDiceNo = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNo.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(onPressed: () {
+              changeDiceFace();
+            },
+              child: Image.asset('images/dice$rightDiceNo.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
